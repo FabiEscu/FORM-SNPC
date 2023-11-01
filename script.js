44c265b7818885fc343ca7a7091338945bcdb182
FabiEscu <144489263+FabiEscu@users.noreply.github.com>
2023-11-01 19:08:13 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+let form = document.createElement('form');
+
+function mostrarValorSeguro() {
+  let valorSeguro = document.getElementById("tipoSeguro").value;
+ 
+  if (valorSeguro == "Basico") {
+    document.getElementById("valorSeguro").innerHTML = "$500";
+  } else if (valorSeguro == "Intermedio") {
+    document.getElementById("valorSeguro").innerHTML = "$1000";
+  } else if (valorSeguro == "Premium") {
+    document.getElementById("valorSeguro").innerHTML = "$1500";
+  }
+}
+
